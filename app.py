588eeecfd8f6de2711466dd4c5de77ba0ea98fbc
NachitoDh <Ignaciojurgen11@gmail.com>
2024-06-23 21:02:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yoelchupa_admin:Nachitodeache11@mysql-yoelchupa.alwaysdata.net/yoelchupa_ecolimdb'
+# Configuración de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://yoelchupa_admin:Nachitodeache11@mysql-yoelchupa.alwaysdata.net/yoelchupa_ecolimdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
+# Definición del modelo de base de datos
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-    correo = db.Column(db.String(100), nullable=True)
     telefono = db.Column(db.String(100), nullable=False)
-    servicio = db.Column(db.String(100), nullable=False)
-    descripcion = db.Column(db.Text, nullable=False)
+    correo = db.Column(db.String(100))
+    descripcion = db.Column(db.String(200), nullable=False)
+    servicio = db.Column(db.String(50), nullable=False)
 
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    data = request.form
-    nuevo_usuario = Usuario(
-        nombre=data.get('nombre'),
-        correo=data.get('correo'),
-        telefono=data.get('telefono'),
-        servicio=data.get('servicio'),
-        descripcion=data.get('descripcion')
-    )
-    db.session.add(nuevo_usuario)
-    db.session.commit()
-    return jsonify({"message": "Datos enviados con éxito"}), 200
+# Crear la tabla en la base de datos
+with app.app_context():
+    db.create_all()
 
+# Ruta para mostrar el formulario
+@app.route('/')
+def formulario():
+    return render_template('formulario.html')
+
+# Ruta para manejar el envío del formulario
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        telefono = request.form['telefono']
+        correo = request.form['correo']
+        descripcion = request.form['descripcion']
+        servicio = request.form['servicio']
+        
+        nuevo_usuario = Usuario(nombre=nombre, telefono=telefono, correo=correo, descripcion=descripcion, servicio=servicio)
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        
+        return redirect('/')
+        
 if __name__ == '__main__':
     app.run(debug=True)
