4edd3ccdcca33119e36a25a8cd524eb9212ee4e4
NachitoDh <Ignaciojurgen11@gmail.com>
2024-06-27 01:38:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ def submit():
         descripcion = request.form['descripcion']
         servicio = request.form['servicio']
 
-        if not re.match(r'^[0-9]+$', telefono):
-            return jsonify({'error': 'El teléfono debe contener solo números'}), 400
+       
             
         nuevo_usuario = Usuario(nombre=nombre, telefono=telefono, correo=correo, descripcion=descripcion, servicio=servicio)
         db.session.add(nuevo_usuario)
